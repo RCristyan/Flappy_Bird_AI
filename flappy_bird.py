41bd1410c7b50d8c484cbd557614ad057a9fde62
RCristyan <rcristyan9@gmail.com>
2020-09-23 15:29:21 -0300

    Adicionado indicador de geraçao.

--- flappy_bird.py
+++ flappy_bird.py
@@ -9,6 +9,8 @@ pygame.font.init()
 WIN_WIDTH = 290
 WIN_HEIGHT = 512
 
+GEN = 0
+
 BIRD_IMGS = [
     pygame.image.load(os.path.join("imgs", "bird1.png")),
     pygame.image.load(os.path.join("imgs", "bird2.png")),
@@ -152,7 +154,7 @@ class Base:
         win.blit(self.IMG, (self.x1, self.y))
         win.blit(self.IMG, (self.x2, self.y))
 
-def draw_window(win, birds, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0,0))
     
     for pipe in pipes:
@@ -161,6 +163,9 @@ def draw_window(win, birds, pipes, base, score):
     text = STAT_FONT.render(f"Score: {str(score)}", 1, (255,255,255))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
 
+    text = STAT_FONT.render(f"Gen: {str(gen)}", 1, (255,255,255))
+    win.blit(text, (10, 10))
+
     base.draw(win)
 
     for bird in birds:
@@ -168,6 +173,8 @@ def draw_window(win, birds, pipes, base, score):
     pygame.display.update()
 
 def main(genomes, config):
+    global GEN
+    GEN += 1
     nets = []
     ge = []
     birds = []
@@ -249,8 +256,11 @@ def main(genomes, config):
                 nets.pop(x)
                 ge.pop(x)
 
+        if score > 30:
+            run = False
+
         base.move()
-        draw_window(win, birds, pipes, base, score)
+        draw_window(win, birds, pipes, base, score, GEN)
 
 def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, 
@@ -266,6 +276,7 @@ def run(config_path):
     p.add_reporter(stats)
 
     winner = p.run(main, 50)
+    print(winner)
 
 if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
